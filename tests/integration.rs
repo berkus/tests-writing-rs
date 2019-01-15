d46e73d489c0b1d62480b37380444135a7cc5b4d
Berkus Decker <berkus+github@metta.systems>
2019-01-15 20:42:28 +0200

    Simplify integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,12 +1,7 @@
-#[cfg(test)]
-mod tests {
-    use tests_writing::libx::x;
-    use tests_writing::liby::y;
-    use tests_writing::test_helper::helper;
+use tests_writing::{libx::x, liby::y, test_helper::helper};
 
-    #[test]
-    fn test_together() {
-        assert_eq!(x() + y(), 66);
-        assert_eq!(helper(), "Hello");
-    }
+#[test]
+fn test_together() {
+    assert_eq!(x() + y(), 66);
+    assert_eq!(helper(), "Hello");
 }
